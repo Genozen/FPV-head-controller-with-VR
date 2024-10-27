73558b5e45abc7069fc2f789c4b718bf947ea4f1
Genozen <74474611+Genozen@users.noreply.github.com>
2024-10-27 12:03:30 -0500

    Add files via upload

--- ArduinoControl.cs
+++ ArduinoControl.cs
@@ -10,8 +10,13 @@ public class ArduinoControl : MonoBehaviour
     SerialPort arduino;
 
 
+    // Create head and both hands controller
     public GameObject headController;
+    public GameObject leftController;
+    public GameObject rightController;
 
+
+    // for computing head
     private float eulerAngX;
     private float eulerAngY;
     private float eulerAngZ;
@@ -23,6 +28,22 @@ public class ArduinoControl : MonoBehaviour
     private float sum_eulerAngX = 0;
     private float sum_eulerAngY = 0;
     private float sum_eulerAngZ = 0;
+
+    // for computing left side arm (hand + shoulder)
+    private float left_eulerAngX;
+    private float left_eulerAngY;
+    private float left_eulerAngZ;
+
+    private float left_avg_eulerAngX = 0;
+    private float left_avg_eulerAngY = 0;
+    private float left_avg_eulerAngZ = 0;
+
+    private float left_sum_eulerAngX = 0;
+    private float left_sum_eulerAngY = 0;
+    private float left_sum_eulerAngZ = 0;
+
+    // TODO: compute right side arm (hand + shoulder)
+
 
     private int counter = 0;
     private int maxCount = 50;
@@ -36,49 +57,87 @@ public class ArduinoControl : MonoBehaviour
 
     void Update()
     {
-        // Debug.Log("Updating");
-
+        // match direction of headset to the robot head
         eulerAngX = -(headController.transform.localEulerAngles.x);
         eulerAngY = headController.transform.localEulerAngles.y;
         eulerAngZ = headController.transform.localEulerAngles.z;
+
+        left_eulerAngX = -(leftController.transform.localEulerAngles.x);
+        left_eulerAngY = leftController.transform.localEulerAngles.y;
+        left_eulerAngZ = leftController.transform.localEulerAngles.z;
 
 
         //Compute for Calibration to set starting as 0 0 0 orienation
         if (counter < maxCount && eulerAngX != 0 && eulerAngY != 0 && eulerAngZ != 0)
         {
-            Debug.Log(eulerAngX + "|" + eulerAngY + "|" + eulerAngZ);
+            //Debug.Log(eulerAngX + "|" + eulerAngY + "|" + eulerAngZ);
             sum_eulerAngX += eulerAngX;
             sum_eulerAngY += eulerAngY;
             sum_eulerAngZ += eulerAngZ;
+
+            left_sum_eulerAngX += left_eulerAngX;
+            left_sum_eulerAngY += left_eulerAngY;
+            left_sum_eulerAngZ += left_eulerAngZ;
+
             counter++;
         }
+        // Reached calibration timer, compute the average, this will be used as initial baseline (0 degree)
         else if (counter == maxCount)
         {
-            Debug.Log(counter);
+            //Debug.Log(counter);
             avg_eulerAngX = sum_eulerAngX / maxCount;
             avg_eulerAngY = sum_eulerAngY / maxCount;
             avg_eulerAngZ = sum_eulerAngZ / maxCount;
-            Debug.Log("avg angle: " + avg_eulerAngX + "|" + avg_eulerAngY + "|" + avg_eulerAngZ);
+
+            left_avg_eulerAngX = left_sum_eulerAngX / maxCount;
+            left_avg_eulerAngY = left_sum_eulerAngY / maxCount;
+            left_avg_eulerAngZ = left_sum_eulerAngZ / maxCount;
+
+
+            //head debug
+            //Debug.Log("Head avg angle: " + avg_eulerAngX + "|" + avg_eulerAngY + "|" + avg_eulerAngZ);
+            //left side debug
+            Debug.Log("Left abg angle: " + left_avg_eulerAngX + "|" + left_avg_eulerAngY + "|" + left_avg_eulerAngZ);
+
             counter++;
         }
         else {
-            //Debug.Log("=====" + (eulerAngX - avg_eulerAngX) + "|" + (eulerAngY - avg_eulerAngY) + "|" + (eulerAngZ - avg_eulerAngZ));
+            // compute current angle to the baseline calculated
             int angX = (int)(eulerAngX - avg_eulerAngX);
+            int left_angX = (int)(left_eulerAngX - left_avg_eulerAngX);
+
+
             // flips the quadrants for continnuity
             if (angX >= 270) {
                 angX = -(360 - angX);
             }
+            if (left_angX >= 270)
+            {
+                left_angX = -(360 - left_angX);
+            }
 
             int angY = (int)(eulerAngY - avg_eulerAngY);
             int angZ = (int)(eulerAngZ - avg_eulerAngZ);
-            Debug.Log("=====" + angX + "|" + angY + "|" + angZ);
 
+            int left_angY = (int)(left_eulerAngY - left_avg_eulerAngY);
+            int left_angZ = (int)(left_eulerAngZ - left_avg_eulerAngZ);
+
+
+            Debug.Log("=== Head: " + angX + "|" + angY + "|" + angZ);
+
+            Debug.Log("=== Left Side: " + left_angX + "|" + left_angY + "|" + left_angZ);
+
+
+
+
+
+            // Wrtie Data to Arduino
             if (arduino.IsOpen)
             {
                 //arduino.WriteLine(angY.ToString());
-                string dataToSend = angX.ToString() + "," + angY.ToString();
+                string dataToSend = angX.ToString() + "," + angY.ToString() + "," + left_angX.ToString() + "," + left_angY.ToString();
                 arduino.WriteLine(dataToSend);
-                Debug.Log("----: " + angY.ToString());
+                //Debug.Log("----: " + angY.ToString());
                 //arduino.Close();
             }
         }
--- FPV_head_controller.ino
+++ FPV_head_controller.ino
@@ -1,9 +1,21 @@
 #include <Servo.h>
 
-Servo servo_1;
-Servo servo_2;
-const int SERVO_PIN1 = 10;
-const int SERVO_PIN2 = 9;
+//create servos
+Servo servo_1; //neck
+Servo servo_2; // eyes
+Servo servo_3; // left shoulder
+Servo servo_4; // left hand
+Servo servo_5; // right shoulder
+Servo servo_6; // right hand
+
+//assign servo pin numbers
+const int SERVO_PIN1 = 10; // neck
+const int SERVO_PIN2 = 9; // eyes
+const int SERVO_PIN3 = 11; // left shoulder
+const int SERVO_PIN4 = 6; // left hand
+const int SERVO_PIN5 = 5; // right shoulder
+const int SERVO_PIN6 = 4; // right hand
+
 int test_angle = -999;
 int compressed_angle = 0;
 
@@ -16,28 +28,46 @@ String temp_String = ",";
 void setup() {
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
+
+  // attach servo to corresponding pin #
   servo_1.attach(SERVO_PIN1);
   servo_2.attach(SERVO_PIN2);
+  servo_3.attach(SERVO_PIN3);
+  servo_4.attach(SERVO_PIN4);
+  servo_5.attach(SERVO_PIN5);
+  servo_6.attach(SERVO_PIN6);
 }
 
 void loop() {
 //  servo_1.write(90);
   if (Serial.available()) {
     String serialData = Serial.readStringUntil('\n');
-    int commaIndex = serialData.indexOf(',');
+    int commaIndex1 = serialData.indexOf(','); // find the first comma
+    int commaIndex2 = serialData.indexOf(',', commaIndex1 + 1); // 2nd comma
+    int commaIndex3 = serialData.indexOf(',', commaIndex2 + 1); // 3rd comma
+    
 
-    if (commaIndex > -1) {
-      String strAngX = serialData.substring(0, commaIndex);
-      String strAngY = serialData.substring(commaIndex + 1);
+    if (commaIndex1 > -1 && commaIndex2 > -1 && commaIndex3 > -1) {
+      // cast the substrings between commas for the actual data
+      String strAngX = serialData.substring(0, commaIndex1);
+      String strAngY = serialData.substring(commaIndex1 + 1, commaIndex2);
+      String left_strAngX = serialData.substring(commaIndex2 + 1, commaIndex3);
+      String left_strAngY = serialData.substring(commaIndex3 + 1);
 
       int angX = strAngX.toInt();
       int angY = strAngY.toInt();
+      int left_angX = left_strAngX.toInt();
+      int left_angY = left_strAngY.toInt();
 
       int compressed_angX = map(angX, 90, -90, 0, 180);
       int compressed_angY = map(angY, 90, -90, 0, 180);
+      int left_compressed_angX = map(left_angX, 90, -90, 0, 180);
+      int left_compressed_angY = map(left_angY, 90, -90, 0, 180);
 
-      servo_1.write(compressed_angY);
-      servo_2.write(compressed_angX);
+      servo_1.write(compressed_angY); // neck
+      servo_2.write(compressed_angX); // eye
+      servo_3.write(left_compressed_angY); // left shoulder
+      servo_4.write(left_compressed_angX); // left hand
       
 //    test_angle = serialData.toInt();
 //    compressed_angle = map(test_angle, 90, -90, 0, 180);
